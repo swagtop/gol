097da3cc73626db433eb040eade03aa7180584f5
thedb <tcj841@alumni.ku.dk>
2024-06-07 01:05:37 +0200

    optimizations, neighbor list and vectors are only allocated once

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-use nannou::prelude::{ App, Update, LoopMode, Frame };
+use nannou::prelude::{ App, Update, Frame };
 use nannou::color::{ BLACK, WHITE };
 use nannou::rand::random_range;
 use nannou::window;
@@ -13,14 +13,19 @@ fn main() {
 struct Model {
     _window: window::Id,
     _cells: HashSet<(i32, i32)>,
+    _kill_list: Vec<(i32, i32)>,
+    _check_list: Vec<(i32, i32)>,
+    _res_list: Vec<(i32, i32)>,
     last_update: Instant,
 }
 
 fn model(app: &App) -> Model {
     let _window = app.new_window().view(view).build().unwrap();
-    app.set_loop_mode(LoopMode::Rate { update_interval: Duration::from_secs(10000)});
 
     let mut _cells: HashSet<(i32, i32)> = HashSet::new();
+    let mut _kill_list: Vec<(i32, i32)> = Vec::new();
+    let mut _check_list: Vec<(i32, i32)> = Vec::new();
+    let mut _res_list: Vec<(i32, i32)> = Vec::new();
 
     let cell_amount = random_range(100, 2000);
     for _ in 0..cell_amount {
@@ -28,43 +33,52 @@ fn model(app: &App) -> Model {
         _cells.insert(cell);
     }
 
-    Model { _window, _cells, last_update: Instant::now() }
+    Model { 
+        _window,
+        _cells, 
+        _kill_list,
+        _check_list,
+        _res_list,
+        last_update: Instant::now() }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
     if _model.last_update.elapsed() < Duration::from_millis(25) { return };
     
-    let mut kill_list = Vec::new();
-    let mut check_list = Vec::new();
-    let mut res_list = Vec::new();
-
     let cells = &mut _model._cells;
+    
+    let kill_list = &mut _model._kill_list;
+    let check_list = &mut _model._check_list;
+    let res_list = &mut _model._res_list;
+
+    let mut neighbor_list: [(i32, i32); 8] = [(0, 0); 8];
 
     for cell in cells.iter() {
-        // Mark dead cells that have the potential for life
-        let neighbor_list = get_neighbors(&cell);
-        for neighbor in neighbor_list.iter() {
-            if cells.contains(neighbor) { continue };
+        // Save list of potential new cells
+        dump_neighbors(&cell, &mut neighbor_list);
+        for neighbor in neighbor_list {
+            if cells.contains(&neighbor) { continue };
             check_list.push(neighbor.clone());       
         }
     
-        // Mark starving cells for death 
-        let neighbor_count: u8 = count_living_neighbors(&cell, &cells);
+        // Save list of cells that should be killed
+        let neighbor_count: u8 = count_living_neighbors(&neighbor_list, &cells);
         if neighbor_count < 2 || neighbor_count > 3 {
             kill_list.push(cell.clone());
         } 
     }
 
-    // Mark deserving cells for life
-    for dead_cell in check_list.iter() {
-        let neighbor_count: u8 = count_living_neighbors(&dead_cell, &cells);
+    // Check if potential cells pass requirements, save list of those that should be given life
+    for dead_cell in check_list.drain(0..) {
+        dump_neighbors(&dead_cell, &mut neighbor_list);
+        let neighbor_count: u8 = count_living_neighbors(&neighbor_list, &cells);
         if neighbor_count == 3 {
-            res_list.push(dead_cell.clone());
+            res_list.push(dead_cell);
         }
     }
 
-    for cell in kill_list { cells.remove(&cell); }  
-    for dead_cell in res_list { cells.insert(dead_cell.clone()); } 
+    for cell in kill_list.drain(0..) { cells.remove(&cell); }  
+    for dead_cell in res_list.drain(0..) { cells.insert(dead_cell); } 
 
     _model.last_update = Instant::now();
 }
@@ -83,24 +97,21 @@ fn view (app: &App, _model: &Model, frame: Frame) {
     draw.to_frame(app, &frame).unwrap();
 }
 
-fn get_neighbors(coordinates: &(i32, i32)) -> Vec<(i32, i32)> {
-    let neighbor_list = Vec::from([
-        (coordinates.0 - 1, coordinates.1 - 1),
-        (coordinates.0,     coordinates.1 - 1),
-        (coordinates.0 + 1, coordinates.1 - 1),
-        (coordinates.0 - 1, coordinates.1    ),
-        (coordinates.0 + 1, coordinates.1    ),
-        (coordinates.0 - 1, coordinates.1 + 1),
-        (coordinates.0,     coordinates.1 + 1),
-        (coordinates.0 + 1, coordinates.1 + 1)
-    ]);
-    neighbor_list
+// Dumps the coordinates of the neighbors of the cells coordinates given
+fn dump_neighbors(coordinates: &(i32, i32), list: &mut [(i32, i32); 8]) {
+    list[0] = (coordinates.0 - 1, coordinates.1 - 1);
+    list[1] = (coordinates.0,     coordinates.1 - 1);
+    list[2] = (coordinates.0 + 1, coordinates.1 - 1);
+    list[3] = (coordinates.0 - 1, coordinates.1    );
+    list[4] = (coordinates.0 + 1, coordinates.1    );
+    list[5] = (coordinates.0 - 1, coordinates.1 + 1);
+    list[6] = (coordinates.0,     coordinates.1 + 1);
+    list[7] = (coordinates.0 + 1, coordinates.1 + 1);
 }
 
-fn count_living_neighbors(coordinates: &(i32, i32), cells: &HashSet<(i32, i32)>) -> u8 {
+fn count_living_neighbors(list: &[(i32, i32); 8], cells: &HashSet<(i32, i32)>) -> u8 {
     let mut count: u8 = 0;
-    let neighbor_list = get_neighbors(&coordinates);
-    for neighbor in neighbor_list.iter() {
+    for neighbor in list {
         if cells.contains(&neighbor) { count += 1 };
     }
     count
